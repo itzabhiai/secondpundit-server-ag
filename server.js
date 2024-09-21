fbf86dfa52612ed6e7364f37adf7688e81782e46
itzabhiai <141840566+itzabhiai@users.noreply.github.com>
2024-09-21 18:43:14 +0530

    Update server.js
    port changes

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/generateCustomToken', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
