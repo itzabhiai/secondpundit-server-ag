1b55deaccb42b479259d80c81f3219733546e56d
itzabhiai <141840566+itzabhiai@users.noreply.github.com>
2024-09-21 18:46:58 +0530

    Update server.js
    nai changes
    port

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/generateCustomToken', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT ||;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
